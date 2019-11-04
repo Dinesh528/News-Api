489994528280731f0454f6dfe556b8a5de0abcb9
Dinesh <petadineshkumar97@gmail.com>
2019-11-04 13:27:15 +0530

    update

--- server.js
+++ server.js
@@ -29,6 +29,15 @@ mongoose.connect(config.DB,{useNewUrlParser: true}).then(
 
 app.use('/',newRoute);
 
+if (process.env.NODE_ENV === 'production') {
+// Set static folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+
+  }
+
 app.listen(PORT, function(){
     console.log('Server is running on http://localhost:4000');
 });
